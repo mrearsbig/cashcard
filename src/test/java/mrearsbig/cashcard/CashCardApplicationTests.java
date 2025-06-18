812184a417b6b8ff9886805925003826d432b326
Alexander Viafara <mrearsbig@gmail.com>
2025-06-18 13:13:57 -0500

    Add integration tests for CashCard with TestRestTemplate

--- src/test/java/mrearsbig/cashcard/CashCardApplicationTests.java
+++ src/test/java/mrearsbig/cashcard/CashCardApplicationTests.java
@@ -1,13 +1,28 @@
 package mrearsbig.cashcard;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CashCardApplicationTests {
+
+	@Autowired
+	TestRestTemplate testRestTemplate;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void shouldReturnACashCardWhenDataIsSaved() {
+		ResponseEntity<String> response = testRestTemplate.getForEntity("/cashcard/1", String.class);
+		// Check if the response status is OK
+		Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
 	}
 
 }
